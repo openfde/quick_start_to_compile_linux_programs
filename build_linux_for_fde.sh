138f4f83349f2673e48c5da88d25c46e2cc0dda1
gongyong <185457686@qq.com>
2024-02-21 11:45:11 +0800

    differentiate unintialized and updating

--- build_linux_for_fde.sh
+++ build_linux_for_fde.sh
@@ -2,74 +2,98 @@
 
 set -e
 
+echo -e "******************installing libglibutil****************************"
 #libglibutil
-sudo apt install git make gcc python3 pkg-config libglib2.0-dev  -y
 if [  ! -e libglibutil ];then
+	sudo apt install git make gcc python3 pkg-config libglib2.0-dev  -y
 	git clone https://gitee.com/openfde/libglibutil.git
+else
 	cd libglibutil 
-	git pull 
-	make
-	sudo make install
-	sudo make install-dev
+	git pull
 	cd -
 fi
+cd libglibutil
+make
+sudo make install
+sudo make install-dev
+cd -
 
 #libgbinder
+echo -e "\n\n\n ******************installing libgbinder****************************"
 if [  ! -e libgbinder ];then
 	git clone https://gitee.com/openfde/libgbinder.git
+else
 	cd libgbinder
 	git pull 
-	make
-	sudo make install
-	sudo make install-dev
 	cd -
 fi
+cd libgbinder
+make
+sudo make install
+sudo make install-dev
+cd -
 
 #gibinder-python
-sudo apt install python3-pip cython3 lxc curl ca-certificates -y
+echo -e "\n\n\n ******************installing gbinder-python****************************"
 if [  ! -e gbinder-python ];then
+	sudo apt install python3-pip cython3 lxc curl ca-certificates -y
 	git clone https://gitee.com/openfde/gbinder-python.git
+	sudo pip3 install pyclip -i https://mirrors.aliyun.com/pypi/simple
+else
 	cd gbinder-python
 	git pull
-	sudo python3 setup.py build_ext --inplace --cython
-	sudo python3 setup.py install
-	sudo pip3 install pyclip -i https://mirrors.aliyun.com/pypi/simple
 	cd -
 fi
+cd gbinder-python
+sudo python3 setup.py build_ext --inplace --cython
+sudo python3 setup.py install
+cd -
 
 #waydroid
+echo  -e "\n\n\n ******************installing waydroid ****************************"
 if [  ! -e waydroid_waydroid ];then
 	git clone https://gitee.com/openfde/waydroid_waydroid.git
+else
 	cd waydroid_waydroid
 	git pull
-	sudo make install
 	cd -
 fi
+cd waydroid_waydroid
+sudo make install
+cd -
 
 #golang1.20.13
-sudo apt install wget -y
 if [ ! -e go1.20.13.linux-arm64.tar.gz ];then
+	echo  -e "\n\n\n ******************installing go ****************************"
+	sudo apt install wget -y
 	wget https://go.dev/dl/go1.20.13.linux-arm64.tar.gz -O ~/go1.20.13.linux-arm64.tar.gz
+	cd ~ && tar -xf ~/go1.20.13.linux-arm64.tar.gz && cd go && sudo cp -a bin/* /usr/bin/
+	sudo sed -i "/GOROOT/d" ~/.bashrc
+	export GOROOT=~/go 
+	echo "export GOROOT=~/go" >> ~/.bashrc
+	sudo sed -i "/GOPATH/d" ~/.bashrc
+	mkdir ~/gopath -p 
+	export GOPATH=~/gopath 
+	echo "export GOPATH=~/gopath" >> ~/.bashrc
+	cd - && go env -w GOPROXY=https://goproxy.cn,direct
 fi
-cd ~ && tar -xf ~/go1.20.13.linux-arm64.tar.gz && cd go && sudo cp -a bin/* /usr/bin/
-sudo sed -i "/GOROOT/d" ~/.bashrc
-export GOROOT=~/go 
-echo "export GOROOT=~/go" >> ~/.bashrc
-sudo sed -i "/GOPATH/d" ~/.bashrc
-mkdir ~/gopath -p 
-export GOPATH=~/gopath 
-echo "export GOPATH=~/gopath" >> ~/.bashrc
-cd - && go env -w GOPROXY=https://goproxy.cn,direct
 
 #fde_fs
+echo -e "\n\n\n ******************building fde_fs****************************"
 sudo apt install  libfuse-dev fuse3 -y
 if [ ! -e fde_fs ];then
 	git clone https://gitee.com/openfde/fde_fs.git
-	cd fde_fs &&  git pull && make
-	sudo make install
-	cd - 
+else
+	cd fde_fs
+	git pull
+	cd -
 fi
+cd fde_fs
+make
+sudo make install
+cd - 
 
+echo  -e "\n\n\n ******************building fde_tigervncserver****************************"
 #tiger_vncserver
 if [ ! -e fde_tigervncserver ];then
 	git clone https://gitee.com/openfde/fde_tigervncserver.git 
@@ -79,48 +103,67 @@ if [ ! -e fde_tigervncserver ];then
 	sudo dpkg -i tigervnc-build-deps_1.10.1+dfsg-3_arm64.deb
 	sudo apt install equivs devscripts --no-install-recommends -y
 	sudo mk-build-deps -i -t "apt-get" -r
-	sudo DEB_BUILD_OPTIONS="parallel=8" dpkg-buildpackage -b -uc -us
-	cd - && sudo apt install libfile-readbackwards-perl
-	sudo dpkg  -i  tigervnc-standalone-server_1.10.1+dfsg-3_arm64.deb
-fi
-
-#fdeime
-sudo apt install libibus-1.0-dev -y
-if [ ! -e fdeime ];then
-	git clone https://gitee.com/openfde/fdeime.git
-	cd fdeime
-	git pull 
-	mkdir build -p && cd build
-	cmake ..
-	make
-	sudo make install
+	cd -
+else
+	cd fde_tigervncserver
+	git pull
 	cd -
 fi
+#cd fde_tigervncserver
+#sudo DEB_BUILD_OPTIONS="parallel=8" dpkg-buildpackage -b -uc -us
+#cd - && sudo apt install libfile-readbackwards-perl
+#sudo dpkg  -i  tigervnc-standalone-server_1.10.1+dfsg-3_arm64.deb
+
+#fdeime
+echo  -e "\n\n\n ******************building fdeime****************************"
+if [ ! -e fdeime ];then
+	git clone https://gitee.com/openfde/fdeime.git
+	sudo apt install libibus-1.0-dev -y
+else
+	cd fdeime
+	git pull 
+	cd -
+fi
+cd fdeime
+mkdir build -p && cd build
+cmake ..
+make
+sudo make install
+cd ../../
 
 
 #mutter
 source /etc/lsb-release
 if [ "$DISTRIB_ID" = "Kylin" ];then
+	echo -e "\n\n\n ******************building mutter****************************"
 	if [ ! -e mutter ];then
 		git clone https://gitee.com/openfde/mutter.git 
+		sudo apt install -y meson libgraphene-1.0-dev libgtk-3-dev gsettings-desktop-schemas-dev gnome-settings-daemon-dev libjson-glib-dev libgnome-desktop-3-dev libxkbcommon-x11-dev libx11-xcb-dev libxcb-randr0-dev libxcb-res0-dev libcanberra-dev libgudev-1.0-dev libinput-dev libstartup-notification0-dev sysprof xwayland gnome-settings-daemon
+	else
 		cd mutter
 		git pull 
-		sudo apt install -y meson libgraphene-1.0-dev libgtk-3-dev gsettings-desktop-schemas-dev gnome-settings-daemon-dev libjson-glib-dev libgnome-desktop-3-dev libxkbcommon-x11-dev libx11-xcb-dev libxcb-randr0-dev libxcb-res0-dev libcanberra-dev libgudev-1.0-dev libinput-dev libstartup-notification0-dev sysprof xwayland gnome-settings-daemon
-		meson build && ninja -C build -j4
-		sudo ninja -C build install && sudo ldconfig
-		cd - 
+		cd -
 	fi
+	cd mutter
+	meson build && ninja -C build -j4
+	sudo ninja -C build install && sudo ldconfig
+	cd - 
 fi
 
 #fde_ctrl
+echo -e "\n\n\n ******************building fde_ctrl****************************"
 if [  ! -e fde_ctrl ];then
 	git clone https://gitee.com/openfde/fde_ctrl.git
-	cd fde_ctrl
-	echo $GOROOT
 	sudo apt install libx11-dev i3 -y
 	sudo rm -rf /usr/share/waydroid-sessions/i3.desktop
-	make build
-       	sudo make install
+else
+	cd fde_ctrl
+	git pull 
+	echo $GOROOT
 	cd -
 fi
+cd fde_ctrl
+make build
+sudo make install
+cd -
 
